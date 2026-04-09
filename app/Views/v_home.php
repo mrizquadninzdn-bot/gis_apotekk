d976d17b9555f70690f1347f16546bef991e8e64
makhdisubulahsalam26-arch <makhdisubulahsalam26@gmail.com>
2026-04-09 23:52:45 +0700

    main

--- app/Views/v_home.php
+++ app/Views/v_home.php
@@ -7,48 +7,49 @@
 <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
 
 <script>
+
+// layer peta
 var peta1 = L.tileLayer(
-'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
+'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
 {
-    attribution: 'Map data © OpenStreetMap contributors, Imagery © Mapbox',
-    id: 'mapbox/streets-v11'
+ attribution:'© OpenStreetMap contributors'
 });
 
 var peta2 = L.tileLayer(
-'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
+'https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
 {
-    attribution: 'Map data © OpenStreetMap contributors, Imagery © Mapbox',
-    id: 'mapbox/satellite-v9'
+ attribution:'© OpenStreetMap contributors'
 });
 
 var peta3 = L.tileLayer(
-'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+'https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png',
 {
-    attribution: '© OpenStreetMap contributors'
+ attribution:'© Stadia Maps'
 });
 
 var peta4 = L.tileLayer(
-'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
+'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png',
 {
-    attribution: 'Map data © OpenStreetMap contributors, Imagery © Mapbox',
-    id: 'mapbox/dark-v10'
+ attribution:'© Stadia Maps'
 });
 
+// tampilkan map
 const map = L.map('map', {
-    center: [-7.239401858334516, 108.98264289428558],
-    zoom: 12,
-    layers: [peta1]
+ center: [-7.239401858334516, 108.98264289428558],
+ zoom: 12,
+ layers: [peta1]
 });
 
+// pilihan layer
 const baseMaps = {
-    "OpenStreetMap": peta1,
-    "Satellite": peta2,
-    "Streets": peta3,
-    "Night": peta4
+ "Standar": peta1,
+ "HOT Map": peta2,
+ "Smooth": peta3,
+ "Dark": peta4
 };
 
-// WAJIB ADA walaupun kosong
 const overlayMaps = {};
 
 L.control.layers(baseMaps, overlayMaps).addTo(map);
+
 </script>
