23569aedad6bfef64e9278d5af9381f75c833c71
makhdisubulahsalam26-arch <makhdisubulahsalam26@gmail.com>
2026-04-10 05:08:16 +0700

    main

--- app/Views/v_home.php
+++ app/Views/v_home.php
@@ -1,55 +1,60 @@
-<div id="map" style="width: 100%; height: 500px;"></div>
+<div id="map" style="width:100%; height:500px;"></div>
 
-<!-- Leaflet CSS -->
-<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"/>
+<link rel="stylesheet"
+href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"/>
 
-<!-- Leaflet JS -->
 <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
 
 <script>
 
-// layer peta
+// standar OSM
 var peta1 = L.tileLayer(
 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
 {
  attribution:'© OpenStreetMap contributors'
 });
 
+// OSM Humanitarian (HOT)
 var peta2 = L.tileLayer(
 'https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
 {
  attribution:'© OpenStreetMap contributors'
 });
 
+// Carto Light
 var peta3 = L.tileLayer(
-'https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png',
+'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
 {
- attribution:'© Stadia Maps'
+ attribution:'© Carto'
 });
 
+// Carto Dark
 var peta4 = L.tileLayer(
-'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png',
+'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
 {
- attribution:'© Stadia Maps'
+ attribution:'© Carto'
 });
 
 // tampilkan map
-const map = L.map('map', {
- center: [-7.239401858334516, 108.98264289428558],
- zoom: 12,
- layers: [peta1]
+const map = L.map('map',{
+ center:[-7.239401858334516,108.98264289428558],
+ zoom:13,
+ layers:[peta1]
 });
 
 // pilihan layer
-const baseMaps = {
- "Standar": peta1,
- "HOT Map": peta2,
- "Smooth": peta3,
- "Dark": peta4
+const baseMaps={
+ "Standar":peta1,
+ "HOT Map":peta2,
+ "Light":peta3,
+ "Dark":peta4
 };
 
-const overlayMaps = {};
+L.control.layers(baseMaps).addTo(map);
 
-L.control.layers(baseMaps, overlayMaps).addTo(map);
+// contoh marker
+L.marker([-7.239401858334516,108.98264289428558])
+.addTo(map)
+.bindPopup("Lokasi Apotek");
 
 </script>
