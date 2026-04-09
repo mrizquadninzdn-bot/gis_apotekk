6e9ecd2a9b95b609bd999efc350f9e6daa036849
makhdisubulahsalam26-arch <makhdisubulahsalam26@gmail.com>
2026-04-09 23:48:10 +0700

    Update v_home.php

--- app/Views/v_home.php
+++ app/Views/v_home.php
@@ -1,26 +1,54 @@
-<div id="map" style="width:100%; height:500px;"></div>
+<div id="map" style="width: 100%; height: 500px;"></div>
 
-<link rel="stylesheet"
-href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"/>
+<!-- Leaflet CSS -->
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"/>
 
+<!-- Leaflet JS -->
 <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
 
 <script>
-
 var peta1 = L.tileLayer(
+'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
+{
+    attribution: 'Map data © OpenStreetMap contributors, Imagery © Mapbox',
+    id: 'mapbox/streets-v11'
+});
+
+var peta2 = L.tileLayer(
+'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
+{
+    attribution: 'Map data © OpenStreetMap contributors, Imagery © Mapbox',
+    id: 'mapbox/satellite-v9'
+});
+
+var peta3 = L.tileLayer(
 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
 {
- attribution:'© OpenStreetMap contributors'
+    attribution: '© OpenStreetMap contributors'
 });
 
-const map = L.map('map',{
- center:[-7.239401858334516,108.98264289428558],
- zoom:13,
- layers:[peta1]
+var peta4 = L.tileLayer(
+'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
+{
+    attribution: 'Map data © OpenStreetMap contributors, Imagery © Mapbox',
+    id: 'mapbox/dark-v10'
 });
 
-L.marker([-7.239401858334516,108.98264289428558])
-.addTo(map)
-.bindPopup("Lokasi Apotek");
+const map = L.map('map', {
+    center: [-7.239401858334516, 108.98264289428558],
+    zoom: 12,
+    layers: [peta1]
+});
 
+const baseMaps = {
+    "OpenStreetMap": peta1,
+    "Satellite": peta2,
+    "Streets": peta3,
+    "Night": peta4
+};
+
+// WAJIB ADA walaupun kosong
+const overlayMaps = {};
+
+L.control.layers(baseMaps, overlayMaps).addTo(map);
 </script>
