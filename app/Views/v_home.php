80493f1c6044e33baaa0b69dfcad60d428618f6b
makhdisubulahsalam26-arch <makhdisubulahsalam26@gmail.com>
2026-04-09 23:08:17 +0700

    main

--- app/Views/v_home.php
+++ app/Views/v_home.php
@@ -1,1 +1,70 @@
-<h1>ini halaman home</h1>
+<div id="map" style="width: 100%; height: 500px;"></div>
+
+<!-- Leaflet CSS -->
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"/>
+
+<!-- Leaflet JS -->
+<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
+
+<script>
+var peta1 = L.tileLayer(
+'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
+{
+    attribution: 'Map data © OpenStreetMap contributors, Imagery © Mapbox',
+    id: 'mapbox/streets-v11'
+});
+
+var peta2 = L.tileLayer(
+'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
+{
+    attribution: 'Map data © OpenStreetMap contributors, Imagery © Mapbox',
+    id: 'mapbox/satellite-v9'
+});
+
+var peta3 = L.tileLayer(
+'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+{
+    attribution: '© OpenStreetMap contributors'
+});
+
+var peta4 = L.tileLayer(
+<div id="map" style="width:100%; height:500px;"></div>
+
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"/>
+<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
+
+<script>
+var peta1 = L.tileLayer(
+'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+{
+    attribution: '&copy; OpenStreetMap contributors'
+});
+
+var peta2 = L.tileLayer(
+'https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
+{
+    attribution: '&copy; OpenStreetMap contributors'
+});
+
+var peta3 = L.tileLayer(
+'https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png',
+{
+    attribution: '&copy; Stadia Maps'
+});
+
+const map = L.map('map', {
+    center: [-7.239401858334516, 108.98264289428558],
+    zoom: 12,
+    layers: [peta1]
+});
+
+const baseMaps = {
+    "Standar": peta1,
+    "HOT Map": peta2,
+    "Smooth": peta3
+};
+
+const overlayMaps = {};
+
+L.control.layers(baseMaps, overlayMaps).addTo(map);
+</script>
