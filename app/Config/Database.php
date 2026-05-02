a3369b78737b4d8a5a7d7bdbc0002322b8dc4708
muhamad zaydan rafi <m.zaydan.r06@gmail.com>
2026-05-02 14:28:49 +0700

    update database

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname'     => 'localhost',
         'username'     => 'root',
         'password'     => '',
-        'database'     => 'db-gis-apotek-ci4',
+        'database'     => 'gis_apotek',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
