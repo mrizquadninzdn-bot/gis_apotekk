41d17227ec33861ee4da3375587d6a15a768af8c
MuhamadZaydanRafi <m.zaydan.r06@gmail.com>
2026-05-04 21:36:00 +0700

    crud jenjang
    membuat fitur jengang

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -18,6 +18,8 @@ $routes->post('Wilayah/InsertData', 'Wilayah::InsertData');
 $routes->get('Wilayah/Edit/(:num)', 'Wilayah::Edit/$1');
 $routes->post('Wilayah/UpdateData/(:num)', 'Wilayah::UpdateData/$1');
 $routes->get('Wilayah/Delete/(:num)', 'Wilayah::Delete/$1');
+$routes->get('Jenjang', 'Jenjang::index');
+$routes->post('Jenjang/UpdateData/(:num)', 'Jenjang::UpdateData/$1');
 
 // Rute Setting
 $routes->get('admin/setting', 'Admin::Setting');
